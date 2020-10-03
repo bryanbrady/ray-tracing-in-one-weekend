2b475ed86670d017d0ddf982d547275409f255ca
Bryan Brady <bryan.brady@gmail.com>
2020-10-03 15:44:15 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ mod vec;
 fn main() -> io::Result<()> {
     let image_width = 200;
     let image_height = 200;
-    println!("P3\n{} {}\n255",image_width, image_height);
     for h in  (0..image_height).rev() {
         // eprintln!("Scanlines remaining: {}", h);
         for w in 0..image_width {
@@ -19,33 +18,6 @@ fn main() -> io::Result<()> {
             color::write_color(&mut io::stdout(), color)?;
         }
     }
-    let a1 = vec::Vec3 { x: 1.0, y: 2.0, z: 3.0 };
-    let a2 = vec::Vec3 { x: 1.0, y: 2.0, z: 3.0 };
-    let aa = a1 + a2;
-    eprintln!("{:?}", aa);
-    eprintln!("{:?}", aa.x);
-    eprintln!("{:?}", aa.y);
-    eprintln!("{:?}", aa.z);
-    let s1 = vec::Vec3 { x: 10.0, y: 20.0, z: 30.0 };
-    let s2 = vec::Vec3 { x: 1.0, y: 2.0, z: 3.0 };
-    let ss = s1 - s2;
-    eprintln!("{:?}", ss);
-    eprintln!("{:?}", ss.x);
-    eprintln!("{:?}", ss.y);
-    eprintln!("{:?}", ss.z);
-    let b1 = vec::Vec3 { x: 10.0, y: 20.0, z: 30.0 };
-    let b2 = 2.0;
-    let bb = b1 / b2;
-    eprintln!("{:?}", bb);
-    eprintln!("{:?}", bb.x);
-    eprintln!("{:?}", bb.y);
-    eprintln!("{:?}", bb.z);
-    let u1 = vec::Vec3 {  x: 1.0, y: 1.0, z: 1.0 };
-    let u2 = vec::Vec3 {  x: 1.0, y: 0.0, z: 0.0 };
-    eprintln!("{:?}", u1.length());
-    eprintln!("{:?}", u2.length());
-
-
     // eprintln!("\nDone!\n");
     Ok(())
 }
