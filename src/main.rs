8f811d0b1ee0466dfa624bb3b7284d7d6663a4f9
Bryan Brady <bryan.brady@gmail.com>
2020-10-04 15:15:05 -0700

    rays, a simple camera, and background

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,50 @@ use std::io::{self};
 
 mod color;
 mod vec;
+mod ray;
+
+
+#[allow(dead_code)]
+fn ray_color(_ray : ray::Ray) -> color::Color {
+    let unit = _ray.direction.unit_vector();
+    let t = 0.5 * (unit.y + 1.0);
+    let c = vec::Vec3{x: 1.0, y: 1.0, z: 1.0} * (1.0-t) + vec::Vec3{x: 0.5, y: 0.7, z: 1.0} * t;
+    color::Color{ r: c.x, g: c.y, b: c.z }
+}
+
 
 fn main() -> io::Result<()> {
-    let image_width = 200;
-    let image_height = 200;
-    for h in  (0..image_height).rev() {
-        // eprintln!("Scanlines remaining: {}", h);
-        for w in 0..image_width {
-            let color = color::Color {
-                r: (w as f64) / (image_width as f64),
-                g: (h as f64) / (image_height as f64),
-                b: 0.2
+
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u64 = 400;
+    const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
+
+    // Camera
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+
+    const ORIG: vec::Vec3 = vec::Vec3{x: 0.0, y: 0.0, z: 0.0};
+    const HORIZONTAL: vec::Vec3  = vec::Vec3{x: VIEWPORT_WIDTH, y: 0.0, z: 0.0};
+    const VERTICAL: vec::Vec3  = vec::Vec3{x: 0.0, y: VIEWPORT_HEIGHT, z: 0.0};
+    let lower_left = ORIG - HORIZONTAL/2.0
+                   - VERTICAL/2.0
+                   - vec::Vec3{x: 0.0, y: 0.0, z: FOCAL_LENGTH};
+
+    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+    for h in  (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", h);
+        for w in 0..IMAGE_WIDTH {
+            let u = (w as f64) / ((IMAGE_WIDTH-1) as f64);
+            let v = (h as f64) / ((IMAGE_HEIGHT-1) as f64);
+            let r = ray::Ray{
+                origin: ORIG,
+                direction: &lower_left + HORIZONTAL*u + VERTICAL*v - ORIG
             };
-            color::write_color(&mut io::stdout(), color)?;
+            color::write_color(&mut io::stdout(), ray_color(r))?;
         }
     }
-    // eprintln!("\nDone!\n");
+    eprintln!("Done!\n");
     Ok(())
 }
