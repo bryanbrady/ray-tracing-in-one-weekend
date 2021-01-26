1cc47a917f6639bb63f059d581d4d4e648790ad5
Bryan Brady <bryan.brady@gmail.com>
2021-01-25 17:19:08 -0800

    add cornell_box_test

--- src/scenes/cornell_box.rs
+++ src/scenes/cornell_box.rs
@@ -27,6 +27,67 @@ pub fn cornell_box(t0: f64, t1: f64, aspect_ratio: f64) -> Scene {
         vup: vec3(0.0, 1.0, 0.0),
         vfov: 40.0,
         aspect_ratio: aspect_ratio,
+        aperture: 0.0,
+        focus_dist: 10.0,
+        time0: t0,
+        time1: t1,
+        background: color(0.0, 0.0, 0.0),
+    });
+
+    let red = Lambertian::new(SolidColor::new(0.65, 0.05, 0.05));
+    let white = Lambertian::new(SolidColor::new(0.73, 0.73, 0.73));
+    let green = Lambertian::new(SolidColor::new(0.12, 0.45, 0.15));
+    let light = Diffuse::new(SolidColor::new(15.0, 15.0, 15.0));
+
+    let wall1 = YzRect::new(0.0, 555.0, 0.0, 555.0, 555.0, green.clone());
+    let wall2 = YzRect::new(0.0, 555.0, 0.0, 555.0, 0.0, red.clone());
+    let wall3 = XzRect::new(0.0, 555.0, 0.0, 555.0, 0.0, white.clone());
+    let wall4 = XzRect::new(0.0, 555.0, 0.0, 555.0, 555.0, white.clone());
+    let wall5 = XyRect::new(0.0, 555.0, 0.0, 555.0, 555.0, white.clone());
+
+    let box1 = Box3D::new(
+        vec3(0.0, 0.0, 0.0),
+        vec3(165.0, 330.0, 165.0),
+        white.clone(),
+    );
+    let box1 = RotateY::new(Arc::new(box1), 15.0);
+    let box1 = Translate::new(Arc::new(box1), vec3(265.0, 0.0, 295.0));
+
+    let box2 = Box3D::new(
+        vec3(0.0, 0.0, 0.0),
+        vec3(165.0, 165.0, 165.0),
+        white.clone(),
+    );
+    let box2 = RotateY::new(Arc::new(box2), -18.0);
+    let box2 = Translate::new(Arc::new(box2), vec3(130.0, 0.0, 65.0));
+
+    let light = XzRect::new(213.0, 343.0, 227.0, 332.0, 554.0, light.clone());
+
+    let mut world = HittableList {
+        hittables: Vec::new(),
+    };
+    world.add(wall1);
+    world.add(wall2);
+    world.add(wall3);
+    world.add(wall4);
+    world.add(wall5);
+    world.add(box1);
+    world.add(box2);
+    world.add(light);
+    return Scene {
+        camera: camera,
+        hittables: Hittables::from(BvhNode::new(world, t0, t1))
+    };
+}
+
+#[allow(dead_code)]
+pub fn cornell_box_test(t0: f64, t1: f64, aspect_ratio: f64) -> Scene {
+    let camera = Camera::new(CameraConfig {
+        lookfrom: vec3(278.0, 278.0, -800.0),
+        lookat: vec3(278.0, 278.0, 0.0),
+        vup: vec3(0.0, 1.0, 0.0),
+        vfov: 40.0,
+        aspect_ratio: aspect_ratio,
         aperture: 0.1,
         focus_dist: 10.0,
         time0: t0,
